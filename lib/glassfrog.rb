87632409a93ba5e8e4bdfe703948fdda137da515
Robert Wells <jrwells10@gmail.com>
2015-06-16 17:28:06 -0400

    add Client class and initialization

--- lib/glassfrog.rb
+++ lib/glassfrog.rb
@@ -7,4 +7,28 @@ module Glassfrog
 
   @@root_uri = 'https://glassfrog.holacracy.org/api/v3/'
 
+  class Client
+
+    def initialize(api_key, *options)
+      @key, @cacheEnabled, @persistEnabled = 
+        api_key, options.include?('cache'), options.include?('persist')
+    end
+
+    def get
+      
+    end
+
+    def post
+      
+    end
+
+    def patch
+      
+    end
+
+    def delete
+      
+    end
+
+  end
 end
