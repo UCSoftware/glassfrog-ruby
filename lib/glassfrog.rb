0ef156e76bf400fbb41804c6361712ca4ca49a76
Robert Wells <jrwells10@gmail.com>
2015-06-16 17:09:30 -0400

    add root uri

--- lib/glassfrog.rb
+++ lib/glassfrog.rb
@@ -1,5 +1,10 @@
 require "glassfrog/version"
 
+require "httparty"
+
 module Glassfrog
-  # Your code goes here...
+  include HTTParty
+
+  @@root_uri = 'https://glassfrog.holacracy.org/api/v3/'
+
 end
