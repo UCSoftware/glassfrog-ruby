70eb6eb5d2b648e937b6f11b1086223712c62c86
Robert Wells <jrwells10@gmail.com>
2015-06-17 16:45:11 -0400

    restructure

--- lib/glassfrog.rb
+++ lib/glassfrog.rb
@@ -13,24 +13,6 @@ module Glassfrog
   @@root_uri = 'https://glassfrog.holacracy.org/api/v3/'
 
   class Client
-    attr_reader :key, :cacheEnabled, :persistEnabled
-
-    def initialize(api_key, *options)
-      @key, @cacheEnabled, @persistEnabled = 
-        api_key, options.include?(:cache), options.include?(:persist)
-      Get.client = Post.client = Patch.client = Delete.client = self
-    end
-    def get(cache)
-      Get::Request.new(cache)
-    end
-    def post
-      Post::Request.new
-    end
-    def patch
-      Patch::Request.new
-    end
-    def delete
-      Delete::Request.new
-    end
+    
   end
 end
